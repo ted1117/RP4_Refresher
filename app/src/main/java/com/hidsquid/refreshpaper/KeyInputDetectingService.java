f7d10060597522cfe205ed818a8ac09ceadd500c
ted111712 <ted111712@gmail.com>
2024-05-25 23:56:40 +0900

    Feat: 리디페이퍼에서 작동 방지

--- app/src/main/java/com/hidsquid/refreshpaper/KeyInputDetectingService.java
+++ app/src/main/java/com/hidsquid/refreshpaper/KeyInputDetectingService.java
@@ -2,6 +2,9 @@ package com.hidsquid.refreshpaper;
 
 
 import android.accessibilityservice.AccessibilityService;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
+import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -9,9 +12,13 @@ import android.view.accessibility.AccessibilityEvent;
 import android.widget.Toast;
 
 import java.lang.reflect.Method;
+import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 public class KeyInputDetectingService extends AccessibilityService {
     private static final String TAG = "MyAccessibilityService";
+    private static final String BLOCKED_APP_PACKAGE_NAME = "com.ridi.paper";
     private int pageUpDownCount = 0;
     private int TRIGGER_COUNT = 5;
     private static final int LONG_PRESS_THRESHOLD = 300;
@@ -38,6 +45,11 @@ public class KeyInputDetectingService extends AccessibilityService {
 
     @Override
     protected boolean onKeyEvent(KeyEvent event) {
+        if (isBlockedAppInForeground()) {
+            Log.d(TAG, "Blocked app is in foreground, ignoring key event.");
+            return false; // 특정 앱이 포그라운드에 있을 때 키 이벤트 무시
+        }
+
         int keyCode = event.getKeyCode();
         int action = event.getAction();
 
@@ -63,9 +75,9 @@ public class KeyInputDetectingService extends AccessibilityService {
                     refreshScreen(1);
                     pageUpDownCount = 0; // reset count after triggering
                 }
-                else {
-                    Toast.makeText(this, String.valueOf(pageUpDownCount), Toast.LENGTH_SHORT).show();
-                }
+//                else {
+//                    Toast.makeText(this, String.valueOf(pageUpDownCount), Toast.LENGTH_SHORT).show();
+//                }
             }
 
         }
@@ -91,5 +103,30 @@ public class KeyInputDetectingService extends AccessibilityService {
         } catch (Exception e) {
             Log.e(TAG, "Reflection error: ", e);
         }
+    }
+
+    private boolean isBlockedAppInForeground() {
+        String foregroundApp = getForegroundApp();
+        return BLOCKED_APP_PACKAGE_NAME.equals(foregroundApp);
+    }
+
+    private String getForegroundApp() {
+        String currentApp = null;
+        UsageStatsManager usm = (UsageStatsManager) this.getSystemService(Context.USAGE_STATS_SERVICE);
+        long time = System.currentTimeMillis();
+        List<UsageStats> appList = usm.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, time - 1000 * 1000, time);
+
+        if (appList != null && !appList.isEmpty()) {
+            SortedMap<Long, UsageStats> sortedMap = new TreeMap<>();
+            for (UsageStats usageStats : appList) {
+                sortedMap.put(usageStats.getLastTimeUsed(), usageStats);
+            }
+
+            if (!sortedMap.isEmpty()) {
+                currentApp = sortedMap.get(sortedMap.lastKey()).getPackageName();
+            }
+        }
+
+        return currentApp;
     }
 }
