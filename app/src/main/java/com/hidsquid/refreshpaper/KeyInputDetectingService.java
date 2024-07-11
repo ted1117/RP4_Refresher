f8c085cef54797305a20cf31d0fc72551c2e7b57
ted111712 <ted111712@gmail.com>
2024-07-11 16:30:57 +0900

    Feat: 수동 새로고침 기능 추가

--- app/src/main/java/com/hidsquid/refreshpaper/KeyInputDetectingService.java
+++ app/src/main/java/com/hidsquid/refreshpaper/KeyInputDetectingService.java
@@ -1,54 +1,48 @@
 package com.hidsquid.refreshpaper;
 
+import static com.hidsquid.refreshpaper.MainUtils.isBlockedAppInForeground;
+import static com.hidsquid.refreshpaper.MainUtils.refreshScreen;
+
 import android.accessibilityservice.AccessibilityService;
-import android.app.usage.UsageStats;
-import android.app.usage.UsageStatsManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.PixelFormat;
 import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
 import android.view.accessibility.AccessibilityEvent;
 
-import java.lang.reflect.Method;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.SortedMap;
-import java.util.TreeMap;
-
+/**
+ * 특정 조건에 따라 키 입력을 감지하고 화면을 새로고침하는 서비스입니다.
+ */
 public class KeyInputDetectingService extends AccessibilityService {
     private static final String TAG = "MyAccessibilityService";
-    private static final String BLOCKED_APP_PACKAGE_NAME = "com.ridi.paper";
     private int pageUpDownCount = 0;
     private int TRIGGER_COUNT = 5;
-    private static final int LONG_PRESS_THRESHOLD = 3000;
-
-    private long keyDownTime;
+    private static final int LONG_PRESS_THRESHOLD = 300;
     public static final String EXTRA_NUMBER = "EXTRA_NUMBER";
     private static final String PREFS_NAME = "MyPrefs";
     private static final String PREF_KEY_PAGES_PER_REFRESH = "numberInput";
-    private MainUtils mainUtils;
+    private static final String PREF_KEY_AUTO_REFRESH_ENABLED = "auto_refresh_enabled";
+    private static final String PREF_KEY_MANUAL_REFRESH_ENABLED = "manual_refresh_enabled";
 
-    private final Set<Integer> keyCodes = new HashSet<>();
-    private final Handler handler = new Handler();
+    private WindowManager windowManager;
+    private View overlayView;
+    private Handler handler;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        // Initialize the set of key codes to track
-        keyCodes.add(KeyEvent.KEYCODE_PAGE_UP);
-        keyCodes.add(KeyEvent.KEYCODE_PAGE_DOWN);
-        keyCodes.add(KeyEvent.KEYCODE_DPAD_LEFT);
-        keyCodes.add(KeyEvent.KEYCODE_DPAD_RIGHT);
-        keyCodes.add(KeyEvent.KEYCODE_VOLUME_UP);
-        keyCodes.add(KeyEvent.KEYCODE_VOLUME_DOWN);
     }
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-
     }
 
     @Override
@@ -59,13 +53,34 @@ public class KeyInputDetectingService extends AccessibilityService {
     protected void onServiceConnected() {
         super.onServiceConnected();
 
-        mainUtils = new MainUtils();
-
         SharedPreferences sharedPreferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
         TRIGGER_COUNT = sharedPreferences.getInt(PREF_KEY_PAGES_PER_REFRESH, 5);
         Log.d(TAG, "onServiceConnected - TRIGGER_COUNT: " + TRIGGER_COUNT);
+
+        // 수동 리프레시를 위해 투명 오버레이 생성
+        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        LayoutInflater inflater = LayoutInflater.from(this);
+
+        overlayView = inflater.inflate(R.layout.overlay_layout, null);
+
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.TYPE_ACCESSIBILITY_OVERLAY,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT);
+        params.gravity = Gravity.TOP | Gravity.START;
+
+        windowManager.addView(overlayView, params);
+        overlayView.setVisibility(View.GONE);
+
+        handler = new Handler(Looper.getMainLooper());
     }
 
+    /**
+     * 자동으로 새로고침할 단위 페이지 수를 TRIGGER_COUNT에 할당합니다.
+     */
+    @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         SharedPreferences sharedPreferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
         if (intent != null && intent.hasExtra(EXTRA_NUMBER)) {
@@ -83,69 +98,79 @@ public class KeyInputDetectingService extends AccessibilityService {
     }
 
     @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (overlayView != null) {
+            windowManager.removeView(overlayView);
+        }
+    }
+
+    @Override
     protected boolean onKeyEvent(KeyEvent event) {
-        if (isBlockedAppInForeground()) {
+        if (isBlockedAppInForeground(this)) {
             Log.d(TAG, "Blocked app is in foreground, ignoring key event.");
             return false;
         }
 
-        int keyCode = event.getKeyCode();
         int action = event.getAction();
 
-//        if (action == KeyEvent.ACTION_DOWN) {
-//            if (keyCodes.contains(keyCode)) {
-//                keyDownTime = System.currentTimeMillis();
-//            }
-//        }
+        if (action == KeyEvent.ACTION_DOWN) {
+            return handleKeyDown(event);
+        }
 
-        if (action == KeyEvent.ACTION_UP) {
-//            long keyPressDuration = System.currentTimeMillis() - keyDownTime;
-//
-//            if (keyPressDuration >= LONG_PRESS_THRESHOLD) {
-//                return true;
-//            }
+        return super.onKeyEvent(event);
+    }
 
-            if (keyCodes.contains(keyCode)) {
-                pageUpDownCount++;
+    /**
+     * 키 코드에 따라 자동·수동으로 화면을 새로고침 합니다.
+     *
+     * @param event 키 이벤트
+     * @return 이벤트가 처리되면 true, 그렇지 않으면 false
+     */
+    private boolean handleKeyDown(KeyEvent event) {
+        int keyCode = event.getKeyCode();
+        int uniqueDrawingId = 1;
 
-                Log.d(TAG, "PageUp/PageDown pressed. Count: " + pageUpDownCount);
+        SharedPreferences sharedPreferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        boolean isAutoRefreshEnabled = sharedPreferences.getBoolean(PREF_KEY_AUTO_REFRESH_ENABLED, false);
+        boolean isManualRefreshEnabled = sharedPreferences.getBoolean(PREF_KEY_MANUAL_REFRESH_ENABLED, false);
 
-                if (pageUpDownCount >= TRIGGER_COUNT) {
-//                    mainUtils.refreshScreen(1);
-                    handler.postDelayed(() -> mainUtils.refreshScreen(1), 500);
-                    pageUpDownCount = 0;
-                }
-//                else {
-//                    Toast.makeText(this, String.valueOf(pageUpDownCount), Toast.LENGTH_SHORT).show();
-//                }
+        if (isAutoRefreshEnabled) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_VOLUME_UP:
+                case KeyEvent.KEYCODE_VOLUME_DOWN:
+                case KeyEvent.KEYCODE_DPAD_RIGHT:
+                case KeyEvent.KEYCODE_DPAD_LEFT:
+                case KeyEvent.KEYCODE_PAGE_UP:
+                case KeyEvent.KEYCODE_PAGE_DOWN:
+                    pageUpDownCount++;
+                    if (pageUpDownCount >= TRIGGER_COUNT) {
+                        refreshScreen(uniqueDrawingId);
+                        pageUpDownCount = 0;
+                    }
+                    return false;
             }
+        }
 
+        if (isManualRefreshEnabled) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_F1:
+                case KeyEvent.KEYCODE_F4:
+                    refreshScreen(uniqueDrawingId);
+                    showOverlay();
+                    return true;
+            }
         }
         return super.onKeyEvent(event);
     }
 
-    private boolean isBlockedAppInForeground() {
-        String foregroundApp = getForegroundApp();
-        return BLOCKED_APP_PACKAGE_NAME.equals(foregroundApp);
+    /**
+     * 일시로 오버레이를 표시합니다.
+     */
+    public void showOverlay() {
+        overlayView.setVisibility(View.VISIBLE);
+        handler.postDelayed(() -> overlayView.setVisibility(View.GONE), 100);
     }
 
-    private String getForegroundApp() {
-        String currentApp = null;
-        UsageStatsManager usm = (UsageStatsManager) this.getSystemService(Context.USAGE_STATS_SERVICE);
-        long time = System.currentTimeMillis();
-        List<UsageStats> appList = usm.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, time - 1000 * 1000, time);
 
-        if (appList != null && !appList.isEmpty()) {
-            SortedMap<Long, UsageStats> sortedMap = new TreeMap<>();
-            for (UsageStats usageStats : appList) {
-                sortedMap.put(usageStats.getLastTimeUsed(), usageStats);
-            }
-
-            if (!sortedMap.isEmpty()) {
-                currentApp = sortedMap.get(sortedMap.lastKey()).getPackageName();
-            }
-        }
-
-        return currentApp;
-    }
 }
--- app/src/main/java/com/hidsquid/refreshpaper/MainActivity.java
+++ app/src/main/java/com/hidsquid/refreshpaper/MainActivity.java
@@ -1,23 +1,18 @@
 package com.hidsquid.refreshpaper;
 
-import android.accessibilityservice.AccessibilityServiceInfo;
-import android.app.AppOpsManager;
-import android.content.Context;
+import static com.hidsquid.refreshpaper.MainUtils.isAccessibilityServiceEnabled;
+import static com.hidsquid.refreshpaper.MainUtils.isPackageUsageStatsEnabled;
+
 import android.content.Intent;
-import android.os.Binder;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
-import android.view.accessibility.AccessibilityManager;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.hidsquid.refreshpaper.databinding.ActivityMainBinding;
-
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -71,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
         binding.accessibilityButton.setVisibility(View.GONE);
         binding.usageStatsButton.setVisibility(View.GONE);
 
-        String[] listItems = {"리프레시할 페이지 숫자 설정"};
+        String[] listItems = {"자동 새로고침 설정", "수동 새로고침 설정"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listItems);
         ListView listView = new ListView(this);
         listView.setAdapter(adapter);
@@ -81,25 +76,10 @@ public class MainActivity extends AppCompatActivity {
             if (position == 0) {
                 Intent intent = new Intent(MainActivity.this, PageCountSettingsActivity.class);
                 startActivity(intent);
+            } else if (position == 1) {
+                Intent intent = new Intent(MainActivity.this, ManualRefreshSettingsActivity.class);
+                startActivity(intent);
             }
         });
-    }
-
-    private boolean isAccessibilityServiceEnabled(Context context) {
-        AccessibilityManager am = (AccessibilityManager) context.getSystemService(Context.ACCESSIBILITY_SERVICE);
-        List<AccessibilityServiceInfo> enabledServices = am.getEnabledAccessibilityServiceList(AccessibilityServiceInfo.FEEDBACK_ALL_MASK);
-        for (AccessibilityServiceInfo enabledService : enabledServices) {
-            if (enabledService.getResolveInfo().serviceInfo.packageName.equals(context.getPackageName()) &&
-                    enabledService.getResolveInfo().serviceInfo.name.equals(KeyInputDetectingService.class.getName())) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private boolean isPackageUsageStatsEnabled(Context context) {
-        AppOpsManager appOpsManager = (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
-        int mode = appOpsManager.unsafeCheckOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS, Binder.getCallingUid(), context.getPackageName());
-        return (mode == AppOpsManager.MODE_ALLOWED);
     }
 }
