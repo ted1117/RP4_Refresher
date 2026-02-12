b1af3531cbc0a9dda39a9f2f2df76c363e1090d4
ted111712 <ted111712@gmail.com>
2026-02-13 00:50:19 +0900

    HookEntry 추가

---
+++ app/src/main/java/com/hidsquid/refreshpaper/hook/HookEntry.kt
@@ -0,0 +1,24 @@
+package com.hidsquid.refreshpaper.hook
+
+import com.highcapable.yukihookapi.annotation.xposed.InjectYukiHookWithXposed
+import com.highcapable.yukihookapi.hook.factory.configs
+import com.highcapable.yukihookapi.hook.factory.encase
+import com.highcapable.yukihookapi.hook.xposed.proxy.IYukiHookXposedInit
+
+@InjectYukiHookWithXposed
+class HookEntry : IYukiHookXposedInit {
+
+    companion object {
+        const val ACTION_REFRESH_SCREEN = "com.hidsquid.refreshpaper.ACTION_REFRESH_SCREEN"
+        const val GLOBAL_KEY_AUTO_REFRESH_ENABLED = "refresh_paper_auto_enabled"
+        const val GLOBAL_KEY_PAGES_PER_REFRESH = "refresh_paper_pages_per_refresh"
+    }
+
+    override fun onInit() = configs { debugLog { tag = "RefreshPaperHook" } }
+
+    override fun onHook() = encase {
+        SystemServerHook.inject(this)
+        AutoRefreshHook.inject(this)
+        RidiSystemUIHook.inject(this)
+    }
+}
